4cdc577224d653770acabd016966813a43b386fb
Jerrod Styrk <jstyrk@citadel.edu>
2020-06-12 11:54:33 -0700

    Create an array up until the input value (This took entirely too long to accomplish

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,17 @@
 // Buisness Logic
 
-// User Logic
+const numberArray = function(createArray) {
+  let emptyArray = [];
+  const numberInput = parseInt($("input#number").val());
+  for (let i=0; i < numberInput; i ++) {
+    emptyArray.push(i+1)
+  }
+  return emptyArray;
+}
+
+
+
+// User Interface Logic
 $(document).ready(function() {
   $("form").submit(function(event) {
     event.preventDefault();
@@ -10,7 +21,7 @@ $(document).ready(function() {
       $("input#number").val();
       return;
     }
-    $("#answer").text(numberInput);
+    $("#answer").text(numberArray);
     $(".answer").show();
   });
 });
