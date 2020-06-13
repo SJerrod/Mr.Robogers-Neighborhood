a40050410e28b35d7fcc720f78d767f2b4afcde5
Jerrod Styrk <jstyrk@citadel.edu>
2020-06-12 17:04:56 -0700

    only display the numbers up till the input

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,29 @@
 // Buisness Logic
 
 const numberArray = function(createArray) {
-  let emptyArray = [0];
+  let emptyArray = [];
   const numberInput = parseInt($("input#number").val());
-  for (let i=0; i < numberInput; i ++) {
-    emptyArray.push(i+1)
+  for (let i=0; i <= numberInput; i ++) {
+    emptyArray.push(i)
   }
   return emptyArray;
 }
 
 
+let result = function(numberArray) {
+  let roboArray = [];
+  for (let i=0; i <= numberArray; i++) {
+    if (roboArray.includes("3")) {
+      return roboArray = "Won't you be my neighbor?";
+    } else if (roboArray.includes("2")) {
+      return roboArray = "Boop!";
+    } else if (roboArray.includes("1")) {
+      return roboArray = "Beep!";
+    } else {
+      return roboArray
+    }
+  }
+}
 
 // User Interface Logic
 $(document).ready(function() {
@@ -21,7 +35,7 @@ $(document).ready(function() {
       $("input#number").val();
       return;
     }
-    $("#answer").text(roboArray);
+    $("#answer").text(result);
     $(".answer").show();
   });
 });
