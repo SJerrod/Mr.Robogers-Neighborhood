44791030796c2cd0dcd38139a622e44a693e1a9e
Jerrod Styrk <jstyrk@citadel.edu>
2020-06-21 11:56:42 -0700

    change the naming so as not to include empty or array

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,20 @@
 // Buisness Logic
 
-const numberArray = function(createArray) {
-  let emptyArray = [];
+const numberList = function(createArray) {
+  let numberOutput = [];
   const numberInput = parseInt($("input#number").val());
   for (let i=0; i <= numberInput; i ++) {
-    emptyArray.push(i)
+    numberOutput.push(i)
   }
-  return emptyArray;
+  return numberOutput;
 }
 
-
-let result = function(numberArray) {
-  let roboArray = [];
-  for (let i=0; i <= numberArray; i++) {
-    if (roboArray.includes("3")) {
-      return roboArray = "Won't you be my neighbor?";
-    } else if (roboArray.includes("2")) {
-      return roboArray = "Boop!";
-    } else if (roboArray.includes("1")) {
-      return roboArray = "Beep!";
-    } else {
-      return roboArray
-    }
+function roboSay(numberList) {
+  let roboOutput = [];
+  for (let i=0; i <= numberList; i++) {
+    numberOutput.push(i);
   }
+  return numberOutput;
 }
 
 // User Interface Logic
@@ -35,7 +27,7 @@ $(document).ready(function() {
       $("input#number").val();
       return;
     }
-    $("#answer").text(result);
+    $("#answer").text(numberList);
     $(".answer").show();
   });
 });
