be54b7413f63858452de0a8694cfee41fbdfe11d
Jerrod Styrk <jstyrk@citadel.edu>
2020-06-21 17:15:00 -0700

    re worked the numberList function, use toString to be able to search elements in array

--- js/scripts.js
+++ js/scripts.js
@@ -1,33 +1,37 @@
 // Buisness Logic
 
-const numberList = function(createArray) {
+function numberList(numberInput) {
   let numberOutput = [];
-  const numberInput = parseInt($("input#number").val());
-  for (let i=0; i <= numberInput; i ++) {
-    numberOutput.push(i)
+  for (i=0; i <= numberInput; i ++) {
+    let roboSay = i.toString();
+    if (roboSay.includes(3)) {
+      numberOutput.push("Won't you be my neighbour?");
+    } else if (roboSay.includes(2)) {
+      numberOutput.push("Boop!");
+    } else if (roboSay.includes(1)) {
+      numberOutput.push("Beep!");
+    } else {
+      numberOutput.push(i);
+    }
   }
+  console.log(numberOutput);
   return numberOutput;
-}
+} 
 
-function roboSay(numberList) {
-  let roboOutput = [];
-  for (let i=0; i <= numberList; i++) {
-    numberOutput.push(i);
-  }
-  return numberOutput;
-}
 
 // User Interface Logic
 $(document).ready(function() {
   $("form").submit(function(event) {
     event.preventDefault();
     const numberInput = parseInt($("input#number").val());
+    let result = numberList(numberInput);
     if(!numberInput || numberInput < 0 ) {
       alert("Incorrect input, please enter a number > 0");
       $("input#number").val();
       return;
     }
-    $("#answer").text(numberList);
+    $("input#number").val("");
+    $("#answer").text(result);
     $(".answer").show();
   });
 });
